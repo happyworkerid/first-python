ae70774975f96e48842e34797abfb8c1c484e03e
happyworkerid <fheppyputri@gmail.com>
2025-05-26 21:56:45 +0700

    my first phyton code!

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 """
-ini adalah demo project pertama dengan Phyton
+Semua sintaksis dasar bahasa pemrograman terdiri dari
+1. Sekuensial : Langkah berurutan
+2. Percabangan : Langkah melompat jika kondisi terpenuhi
+3. Perulangan : Mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
-print("Hello World!")
-print("My name is Heppy")
+
+# Sekuensial
+print('Ibu berkata, "Pergi ke toko"')
+print('Budi menjawab, "Baik, apa yang harus saya beli?"')
+print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
+print("Maka Budi berangkat ke toko")
+print("Dan mulai berbelanja")
+
+# Percabangan
+jumlah_botol_susu = 173
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi pulang kerumah")
+print("Budi menyampaikan hasil belanja kepada Ibu")
+
+
