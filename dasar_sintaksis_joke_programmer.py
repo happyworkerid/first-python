a86e2d1edc8c589d36c704814e8530fc94b3e803
happyworkerid <fheppyputri@gmail.com>
2025-06-01 20:23:48 +0700

    sekuensial dan percabangan sequential and branching

--- dasar_sintaksis_joke_programmer.py
+++ dasar_sintaksis_joke_programmer.py
@@ -14,10 +14,17 @@ print("Dan mulai berbelanja")
 
 # Percabangan
 jumlah_botol_susu = 173
+jumlah_telur = 1587
+print(f"Jumlah botol susu {jumlah_botol_susu} botol")
+print(f"Jumlah telur {jumlah_telur} butir")
 
 if jumlah_botol_susu > 0:
     print("Budi mengecek harganya, dan ternyata uangnya cukup")
-    print("Budi membeli 1 botol susu")
+    if jumlah_telur == 0:
+        print("Budi membeli 1 botol susu")
+    else:
+        print("Budi membeli 6 botol susu")
+
 else:
     print("Budi tidak jadi membeli 1 botol susu")
 
